4a3277a99aa1bc253e672970e42f4dae1581da6e
Stone Chen <thestonechen@gmail.com>
2021-02-27 20:10:51 -1000

    Initial commit

---
+++ eyeCarousel/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eyeCarousel
+//
+//  Created by Stone Chen on 2/27/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
